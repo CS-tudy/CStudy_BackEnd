e0b91be8188066ff388b69b82301d4f46f80a7cb
charon <pos04167@kakao.com>
2023-08-24 23:52:34 +0900

    ✨feat: GeoIp 추가

---
+++ module-api/src/main/java/com/cstudy/moduleapi/dto/ipAddress/GeoLocationDto.java
@@ -0,0 +1,59 @@
+package com.cstudy.moduleapi.dto.ipAddress;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class GeoLocationDto {
+    private String subdivisionName;
+    private String cityName;
+    private String continentName;
+    private String countryName;
+    private String countryIsoCode;
+    private String hostAddress;
+
+    public static InetAddress getIpAddress() {
+        HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+
+        String ip = req.getHeader("X-FORWARDED-FOR");
+
+        if (ip == null || ip.isEmpty()) {
+            ip = req.getRemoteAddr();
+        }
+        if (ip == null || ip.isEmpty()) {
+            ip = req.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty()) {
+            ip = req.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.isEmpty()) {
+            ip = req.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.isEmpty()) {
+            ip = req.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.isEmpty()) {
+            ip = req.getRemoteAddr();
+        }
+        if (ip == null || ip.isEmpty()) {
+            throw new RuntimeException();
+        }
+
+        InetAddress ipAddress = null;
+        try {
+            ipAddress = InetAddress.getByName(ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return ipAddress;
+    }
+}
