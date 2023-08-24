bd621db144794a59e2ae800f8f8b754acad6c22c
charon <pos04167@kakao.com>
2023-08-24 23:50:55 +0900

    ✅test: 테스트 code 추가

---
+++ module-common/src/test/java/com/cstudy/modulecommon/repository/competition/MemberCompetitionRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.cstudy.modulecommon.repository.competition;
+
+import com.cstudy.modulecommon.config.QueryDslConfig;
+import com.cstudy.modulecommon.domain.competition.Competition;
+import com.cstudy.modulecommon.domain.competition.MemberCompetition;
+import com.cstudy.modulecommon.domain.member.Member;
+import com.cstudy.modulecommon.repository.member.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.time.LocalDateTime;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({QueryDslConfig.class})
+class MemberCompetitionRepositoryTest {
+
+    @Autowired
+    private MemberCompetitionRepository memberCompetitionRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private CompetitionRepository competitionRepository;
+
+    @Test
+    public void testFindAllWithMemberAndCompetition() {
+
+        memberRepository.save(Member.builder()
+                .email("test@example.com")
+                .password("test1234!")
+                .name("김무건")
+                .build());
+
+        LocalDateTime now = LocalDateTime.now();
+
+        Competition competition =Competition.builder()
+                .competitionTitle("제목")
+                .competitionStart(now)
+                .competitionEnd(now.plusHours(1))
+                .participants(5)
+                .build();
+
+        competitionRepository.save(competition);
+
+        MemberCompetition memberCompetition = MemberCompetition.builder()
+                .member(Member.builder()
+                        .email("test@example.com")
+                        .password("test1234!")
+                        .name("김무건")
+                        .build())
+                .competition(competition)
+                .build();
+
+        memberCompetitionRepository.save(memberCompetition);
+    }
+}
