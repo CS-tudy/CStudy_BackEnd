2cf04b060f65afa02ae921349fb86f1b8b485542
charon <pos04167@kakao.com>
2023-09-01 01:30:13 +0900

    refactor : 문제집 파일 업로드, 이미지 Path

--- module-common/src/main/java/com/cstudy/modulecommon/repository/workbook/WorkbookRepository.java
+++ module-common/src/main/java/com/cstudy/modulecommon/repository/workbook/WorkbookRepository.java
@@ -3,7 +3,12 @@ package com.cstudy.modulecommon.repository.workbook;
 
 import com.cstudy.modulecommon.domain.workbook.Workbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WorkbookRepository extends JpaRepository<Workbook, Long>, WorkbookRepositoryCustom {
 
+    @Query("SELECT w FROM Workbook w JOIN FETCH w.files f")
+    List<Workbook> findByIdWithWorkbook();
 }
