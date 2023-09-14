9f22eac1cb761e54b5d1c64a62d8abaa3ade674b
charon <pos04167@kakao.com>
2023-09-14 22:55:33 +0900

    refactor : querydsl 변경

--- module-common/src/main/java/com/cstudy/modulecommon/dto/NoticeResponseDto.java
+++ module-common/src/main/java/com/cstudy/modulecommon/dto/NoticeResponseDto.java
@@ -1,5 +1,7 @@
 package com.cstudy.modulecommon.dto;
 
+import com.cstudy.modulecommon.domain.member.Member;
+import com.cstudy.modulecommon.domain.notice.Notice;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Data;
@@ -11,12 +13,26 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 public class NoticeResponseDto {
+    private Long id;
     private String title;
     private String content;
+    private LocalDateTime createdDate;
 
     @QueryProjection
-    public NoticeResponseDto(String title, String content) {
+
+    public NoticeResponseDto(Long id, String title, String content, LocalDateTime createdDate) {
+        this.id = id;
         this.title = title;
         this.content = content;
+        this.createdDate = createdDate;
+    }
+
+    public static NoticeResponseDto of(Notice notice) {
+        return NoticeResponseDto.builder()
+                .id(notice.getId())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .createdDate(notice.getCreatedDate())
+                .build();
     }
 }
