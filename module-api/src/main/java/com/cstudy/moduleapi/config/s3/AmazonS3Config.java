422798ff1865487586d7f54517a326c2d3cea34c
charon <pos04167@kakao.com>
2023-10-03 14:46:10 +0900

    refactor : AWS Bean 2개 주입 수정

---
+++ module-api/src/main/java/com/cstudy/moduleapi/config/s3/AmazonS3Config.java
@@ -0,0 +1,31 @@
+package com.cstudy.moduleapi.config.s3;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonS3Config {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private  String S3_accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String S3_secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 s3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(S3_accessKey, S3_secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region).build();
+    }
+}
