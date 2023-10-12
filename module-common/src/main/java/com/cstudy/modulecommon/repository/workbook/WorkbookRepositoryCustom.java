54654419b8347a922da0f568030852bb22e43904
charon <pos04167@kakao.com>
2023-10-12 23:21:57 +0900

    refactor : workbook에 이미지를 같이 response dto에 수정

--- module-common/src/main/java/com/cstudy/modulecommon/repository/workbook/WorkbookRepositoryCustom.java
+++ module-common/src/main/java/com/cstudy/modulecommon/repository/workbook/WorkbookRepositoryCustom.java
@@ -2,12 +2,13 @@ package com.cstudy.modulecommon.repository.workbook;
 
 import com.cstudy.modulecommon.dto.WorkbookQuestionResponseDto;
 import com.cstudy.modulecommon.dto.WorkbookResponseDto;
+import com.cstudy.modulecommon.dto.WorkbookSearchRequestDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface WorkbookRepositoryCustom {
 
-    Page<WorkbookResponseDto> findWorkbookList(Pageable pageable, String title, String description, String titleDesc);
+    Page<WorkbookResponseDto> findWorkbookList(Pageable pageable, WorkbookSearchRequestDto requestDto);
 
     Page<WorkbookQuestionResponseDto> findWorkbookQuestionList(Pageable pageable, Long id);
 }
