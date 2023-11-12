74fada6ba7a2a2d781e23b8334350fab5b49da53
charon <pos04167@kakao.com>
2023-11-12 20:08:37 +0900

    feat : 기존의 queryDsl 코드를 nativeQuery로 List<Object[]>로 가져와서 Dto 매핑

--- module-common/src/main/java/com/cstudy/modulecommon/repository/competition/CompetitionRepository.java
+++ module-common/src/main/java/com/cstudy/modulecommon/repository/competition/CompetitionRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.repository.query.Param;
 
 import javax.persistence.LockModeType;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface CompetitionRepository extends JpaRepository<Competition, Long> {
@@ -22,4 +23,29 @@ public interface CompetitionRepository extends JpaRepository<Competition, Long> 
     @Query("select m from Competition m where m.id = :id")
     Optional<Competition> findByIdForUpdateOptimistic(@Param("id") Long id);
 
+
+    @Query(value = "SELECT " +
+            "    question0_.question_id as question_id, " +
+            "    question0_.question_description as question_description, " +
+            "    GROUP_CONCAT(choices4_.choice_number) as choice_numbers, " +
+            "    GROUP_CONCAT(choices4_.content) as choice_contents " +
+            "FROM " +
+            "    question question0_ " +
+            "LEFT OUTER JOIN " +
+            "    workbook_question workbookqu1_ " +
+            "        ON question0_.question_id=workbookqu1_.question_id " +
+            "LEFT OUTER JOIN " +
+            "    workbook workbook2_ " +
+            "        ON workbookqu1_.workbook_id=workbook2_.workbook_id " +
+            "LEFT OUTER JOIN " +
+            "    competition competitio3_ " +
+            "        ON workbook2_.workbook_id=competitio3_.workbook_id " +
+            "LEFT OUTER JOIN " +
+            "    choice choices4_ " +
+            "        ON question0_.question_id=choices4_.question_id " +
+            "WHERE " +
+            "    competitio3_.competition_id=:competitionId " +
+            "GROUP BY " +
+            "    question0_.question_id", nativeQuery = true)
+    List<Object[]> findQuestionsWithChoices(@Param("competitionId") Long competitionId);
 }
