27c05824b388a5cdde9f1cf7a5d6fbd974b94499
charon <pos04167@kakao.com>
2023-11-30 20:09:49 +0900

    refactor : jwt token exception 분기문에 따른 filter 반환값 일정하게 변경

--- module-api/src/main/java/com/cstudy/moduleapi/config/jwt/filter/JwtAuthenticationFilter.java
+++ module-api/src/main/java/com/cstudy/moduleapi/config/jwt/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,9 @@
 package com.cstudy.moduleapi.config.jwt.filter;
 
 
+import com.cstudy.moduleapi.config.jwt.exception.CustomAccessDeniedHandler;
 import com.cstudy.moduleapi.config.jwt.exception.JwtExceptionCode;
 import com.cstudy.moduleapi.config.jwt.token.JwtAuthenticationToken;
-import com.cstudy.moduleapi.config.jwt.util.JwtTokenizer;
-import com.cstudy.modulecommon.repository.member.MemberRepository;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
@@ -21,58 +20,48 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final AuthenticationManager authenticationManager;
-    private final MemberRepository memberRepository;
-    private final JwtTokenizer jwtTokenizer;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
-    ) throws ServletException, IOException {
-
-        String token = "";
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String token="";
         try {
             token = getToken(request);
             if (StringUtils.hasText(token)) {
                 getAuthentication(token);
-            } else {
-                handleJwtException(request, JwtExceptionCode.NOT_FOUND_TOKEN, "Not found Token // token : {}", token, "throw new not found token exception");
             }
-        } catch (NullPointerException | IllegalStateException e) {
-            handleJwtException(request, JwtExceptionCode.NOT_FOUND_TOKEN, "Not found Token // token : {}", token, "throw new not found token exception");
-        } catch (SecurityException | MalformedJwtException e) {//JWT 토큰의 서명이나 암호화와 관련된 오류입니다. -> 시그니처 오류
-            handleJwtException(request, JwtExceptionCode.INVALID_TOKEN, "Invalid Token // token : {}", token, "throw new invalid token exception");
-        } catch (ExpiredJwtException e) {//JWT 토큰의 만료 시간이 지났을 때 발생하는 오류입니다.
-            handleJwtException(request, JwtExceptionCode.EXPIRED_TOKEN, "EXPIRED Token // token : {}", token, "throw new expired token exception");
-        } catch (UnsupportedJwtException e) {//지원되지 않는 JWT 토큰 형식이나 구조를 사용했을 때 발생하는 오류입니다.
-            handleJwtException(request, JwtExceptionCode.UNSUPPORTED_TOKEN, "Unsupported Token // token : {}", token, "throw new unsupported token exception");
-        } catch (Exception e) {
-//            log.error("JwtFilter - doFilterInternal() 오류 발생");
-//            log.error("token : {}", token);
-//            log.error("Exception Message : {}", e.getMessage());
-            //throw new BadCredentialsException("throw new exception");
+            filterChain.doFilter(request, response);
         }
-        filterChain.doFilter(request, response);
+        catch (NullPointerException | IllegalStateException e) {
+            request.setAttribute("exception", JwtExceptionCode.NOT_FOUND_TOKEN.getCode());
+            customAccessDeniedHandler.handle(request,response);
+        } catch (SecurityException | MalformedJwtException e) {
+            request.setAttribute("exception", JwtExceptionCode.INVALID_TOKEN.getCode());
+            customAccessDeniedHandler.handle(request,response);
+        } catch (ExpiredJwtException e) {
+            request.setAttribute("exception", JwtExceptionCode.EXPIRED_TOKEN.getCode());
+            customAccessDeniedHandler.handle(request,response);
+        } catch (UnsupportedJwtException e) {
+            request.setAttribute("exception", JwtExceptionCode.UNSUPPORTED_TOKEN.getCode());
+            customAccessDeniedHandler.handle(request,response);
+        } catch (Exception e) {
+            customAccessDeniedHandler.handle(request,response);
+        }
     }
 
-    private static void handleJwtException(HttpServletRequest request, JwtExceptionCode notFoundToken, String format, String token, String msg) {
-        request.setAttribute("exception", notFoundToken.getCode());
-//        log.error(format, token);
-//        log.error("Set Request Exception Code : {}", request.getAttribute("exception"));
-        //throw new BadCredentialsException(msg);
-    }
 
     private void getAuthentication(String token) {
         JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(token);
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authenticate); // 현재 요청에서 언제든지 인증정보를 꺼낼 수 있도록 해준다.
+
+        SecurityContextHolder.getContext()
+                .setAuthentication(authenticate); // 현재 요청에서 언제든지 인증정보를 꺼낼 수 있도록 해준다.
     }
 
     private String getToken(HttpServletRequest request) {
